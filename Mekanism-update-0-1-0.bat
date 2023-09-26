8ce0712285c9287571384e203e969261a77e3696
Jakob <37959554+GenuineJakob@users.noreply.github.com>
2023-09-26 16:53:21 +0200

    Create Mekanism-update-0-1-0.bat

---
+++ Mekanism-update-0-1-0.bat
@@ -0,0 +1,26 @@
+del "create_enchantment_industry*.jar"
+curl "https://mediafilez.forgecdn.net/files/4764/837/create_enchantment_industry-1.19.2-for-create-0.5.1.e-1.2.6.c.jar" --output "create_enchantment_industry-1.19.2-for-create-0.5.1.e-1.2.6.c.jar"
+
+del "create-1.19.2-0.5*"
+curl "https://mediafilez.forgecdn.net/files/4762/215/create-1.19.2-0.5.1.e.jar" --output "create-1.19.2-0.5.1.e.jar"
+
+del "CreateTweaker*.jar"
+curl "https://mediafilez.forgecdn.net/files/4764/75/CreateTweaker-1.19.2-3.0.0.7.jar" --output "CreateTweaker-1.19.2-3.0.0.7.jar"
+
+del "mcw-windows*.jar"
+curl "https://mediafilez.forgecdn.net/files/4750/673/mcw-windows-2.2.0-mc1.19.2forge.jar" --output "mcw-windows-2.2.0-mc1.19.2forge.jar"
+
+del "CraftTweaker*.jar"
+curl "https://mediafilez.forgecdn.net/files/4764/852/CraftTweaker-forge-1.19.2-10.1.50.jar" --output "CraftTweaker-forge-1.19.2-10.1.50.jar.jar"
+
+del "moonlight*.jar"
+curl "https://mediafilez.forgecdn.net/files/4758/15/moonlight-1.19.2-2.2.46-forge.jar" --output "moonlight-1.19.2-2.2.46-forge.jar"
+
+del "createaddition*.jar"
+curl "https://mediafilez.forgecdn.net/files/4767/406/createaddition-1.19.2-1.1.1.jar" --output "createaddition-1.19.2-1.1.1.jar"
+
+del "rubidium*"
+curl "https://mediafilez.forgecdn.net/files/4763/255/rubidium-mc1.19.2-0.6.2c.jar" --output "rubidium-mc1.19.2-0.6.2c.jar"
+
+del "oculus*"
+curl "https://mediafilez.forgecdn.net/files/4763/262/oculus-mc1.19.2-1.6.9.jar" --output "oculus-mc1.19.2-1.6.9.jar"
