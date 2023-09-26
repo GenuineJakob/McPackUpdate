bc44cd766016461d340fcbb7702a1f8c0a94f9de
Jakob <37959554+GenuineJakob@users.noreply.github.com>
2023-09-26 15:20:20 +0200

    Create Mekanisum-update-0-1-0.bat

---
+++ Mekanisum-update-0-1-0.bat
@@ -0,0 +1,34 @@
+del "create_enchantment_industry-1.19.2-for-create-0.5.1.c-1.2.6.b.jar"
+curl "https://mediafilez.forgecdn.net/files/4764/837/create_enchantment_industry-1.19.2-for-create-0.5.1.e-1.2.6.c.jar" --output "create_enchantment_industry-1.19.2-for-create-0.5.1.e-1.2.6.c.jar"
+
+del "create-1.19.2-0.5.1.c.jar"
+curl "https://mediafilez.forgecdn.net/files/4762/215/create-1.19.2-0.5.1.e.jar" --output "create-1.19.2-0.5.1.e.jar"
+
+del "creategoggles-1.19.2-0.5.5.d-[FORGE].jar"
+curl "https://mediafilez.forgecdn.net/files/4765/53/creategoggles-1.19.2-0.5.5.d-%5bFORGE%5d.jar" --output "creategoggles-1.19.2-0.5.5.d-[FORGE].jar"
+
+del "CreateTweaker-1.19.2-3.0.0.6.jar"
+curl "https://mediafilez.forgecdn.net/files/4764/75/CreateTweaker-1.19.2-3.0.0.7.jar" --output "CreateTweaker-1.19.2-3.0.0.7.jar"
+
+del "mcw-windows-2.1.1-mc1.19.2forge.jar"
+curl "https://mediafilez.forgecdn.net/files/4750/673/mcw-windows-2.2.0-mc1.19.2forge.jar" --output "mcw-windows-2.2.0-mc1.19.2forge.jar"
+
+del "CraftTweaker-forge-1.19.2-10.1.49.jar"
+curl "https://mediafilez.forgecdn.net/files/4764/852/CraftTweaker-forge-1.19.2-10.1.50.jar" --output "CraftTweaker-forge-1.19.2-10.1.50.jar.jar"
+
+del "moonlight-1.19.2-2.2.44-forge.jar"
+curl "https://mediafilez.forgecdn.net/files/4758/15/moonlight-1.19.2-2.2.46-forge.jar" --output "moonlight-1.19.2-2.2.46-forge.jar"
+
+del "createaddition-1.19.2-1.0.0.jar"
+curl "https://mediafilez.forgecdn.net/files/4767/406/createaddition-1.19.2-1.1.1.jar" --output "createaddition-1.19.2-1.1.1.jar"
+
+del "cobblegen-5.0.3+1.19.2b25-BETA-forge.jar"
+curl "https://cdn-raw.modrinth.com/data/qRogT5Gy/versions/TB1lXWjm/cobblegen-5.0.3%2B1.19.2b25-BETA-forge.jar" --output "cobblegen-5.0.3+1.19.2b25-BETA-forge.jar"
+
+del "rubidium*"
+curl "https://mediafilez.forgecdn.net/files/4763/255/rubidium-mc1.19.2-0.6.2c.jar" --output "rubidium-mc1.19.2-0.6.2c.jar"
+
+del "oculus*"
+curl "https://mediafilez.forgecdn.net/files/4763/262/oculus-mc1.19.2-1.6.9.jar" --output "oculus-mc1.19.2-1.6.9.jar"
+
+del "del "0-1-0-update.bat"
